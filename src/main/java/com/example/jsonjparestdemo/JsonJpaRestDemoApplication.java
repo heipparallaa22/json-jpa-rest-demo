830a2a2fbf8a0b3bacc00467bfd5b53af36d4699
katitmukaan <unto.j.valkonen@icloud.com>
2023-03-25 18:08:20 +0200

    commit

---
+++ src/main/java/com/example/jsonjparestdemo/JsonJpaRestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jsonjparestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonJpaRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonJpaRestDemoApplication.class, args);
+	}
+
+}
